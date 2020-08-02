63bc857936b65318973714161ba0d3505ead0fad
cpsingh111 <68427957+cpsingh111@users.noreply.github.com>
2020-08-02 14:36:48 +0530

    Added test cases for Status code check

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -6,9 +6,24 @@ TEST(StatusCode, ok) {
     ASSERT_EQ(false, Notify_StatusCode(0xFF));
 }
 
-TEST(StatusCode, Nok) { 
-    ASSERT_EQ(false, Notify_StatusCode(0x00));
+TEST(StatusCode, Empty) { 
+    ASSERT_EQ(true, Notify_StatusCode(0x00));
 }
+
+
+TEST(StatusCode, ControllerBoard) { 
+    ASSERT_EQ(true, Notify_StatusCode(0x01));
+}
+
+
+TEST(StatusCode, Config data) { 
+    ASSERT_EQ(true, Notify_StatusCode(0x02));
+}
+
+TEST(StatusCode, Undefined) { 
+    ASSERT_EQ(true, Notify_StatusCode(0x02));
+}
+
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
