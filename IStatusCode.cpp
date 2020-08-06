090204e7f27c12c8e30695188c6ac020a45dcd58
cpsingh111 <68427957+cpsingh111@users.noreply.github.com>
2020-08-06 18:18:25 +0530

    Syntax errors

--- IStatusCode.cpp
+++ IStatusCode.cpp
@@ -6,6 +6,6 @@ CNC_Status Notify_StatusCode(int val)
     ICNC Obj;
 
     temp.Set_StatusCode(val);
-    Obj.StartUpStatusflag =temp.IsStatusCodeNOk()
+    Obj.StartUpStatusflag =temp.IsStatusCodeNOk();
     return(Report_Diagnosis());
 }
