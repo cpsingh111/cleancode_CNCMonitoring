452874d104b5822776da64f328ad4f5ee1bb6ff3
cpsingh111 <68427957+cpsingh111@users.noreply.github.com>
2020-08-07 14:09:08 +0530

    Setter func for param flags

--- IOperatingTemperature.cpp
+++ IOperatingTemperature.cpp
@@ -7,7 +7,7 @@ CNC_Status Notify_OperatingTemperature(float val)
     ICNC Obj;
 
     temp.Set_OperatingTemperature(val);
-    Obj.OperatingTempflag =temp.IsOperatingTemperatureNOk();
+    Obj.Set_CNC_Parameter_flag(OperatingTempflag,temp.IsOperatingTemperatureNOk());
     return(Report_Diagnosis());
 
 }
