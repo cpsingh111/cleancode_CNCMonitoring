f98db37ccddb5b03ece3e551cfcc275071489d46
cpsingh111 <68427957+cpsingh111@users.noreply.github.com>
2020-08-07 14:10:57 +0530

    Validator functions for Environment and Machine Check

---
+++ IValidator.cpp
@@ -0,0 +1,18 @@
+#include "IValidator.hpp"
+
+
+bool Validate_Environment_Param()
+{
+    ICNC Obj;
+ 
+    return(Obj.Get_CNC_Parameter_flag(OperatingHourflag)||Obj.Get_CNC_Parameter_flag(OperatingTempflag));
+
+}
+
+bool Validate_Machine_Param()
+{
+    ICNC Obj;
+ 
+    return(Obj.Get_CNC_Parameter_flag(StartUpStatusflag)||Obj.Get_CNC_Parameter_flag(DimensionVariationflag));
+
+}
---
+++ IValidator.hpp
@@ -0,0 +1,9 @@
+#ifndef IVALIDATOR_H
+#define IVALIDATOR_H
+
+#include "ICNC.hpp"
+
+bool Validate_Environment_Param();
+bool Validate_Machine_Param();
+
+#endif
