68c0f8bd534f6911b4214d16a7dfa163f372aca3
cpsingh111 <68427957+cpsingh111@users.noreply.github.com>
2020-08-07 17:29:02 +0530

    Test case for DimensionVariation added

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -1,28 +1,39 @@
 #include "IStatusCode.hpp"
+#include "ICNC.hpp"
 #include <gtest/gtest.h>
+#include "IDimensionVariation.hpp"
 
 
 TEST(StatusCode, ok) { 
-    ASSERT_EQ(false, Notify_StatusCode(0xFF));
+    ASSERT_EQ(CNC_Ok, Notify_StatusCode(0xFF));
 }
 
 TEST(StatusCode, Empty) { 
-    ASSERT_EQ(true, Notify_StatusCode(0x00));
+    ASSERT_EQ(CNC_CheckMachine, Notify_StatusCode(0x00));
 }
 
 
 TEST(StatusCode, ControllerBoard) { 
-    ASSERT_EQ(true, Notify_StatusCode(0x01));
+    ASSERT_EQ(CNC_CheckMachine, Notify_StatusCode(0x01));
 }
 
 
 TEST(StatusCode, Config_data) { 
-    ASSERT_EQ(true, Notify_StatusCode(0x02));
+    ASSERT_EQ(CNC_CheckMachine, Notify_StatusCode(0x02));
 }
 
 TEST(StatusCode, Undefined) { 
-    ASSERT_EQ(true, Notify_StatusCode(0x02));
+    ASSERT_EQ(CNC_CheckMachine, Notify_StatusCode(0x03));
 }
+
+TEST(DImensionVariation, Ok) { 
+    ASSERT_EQ(CNC_Ok, Notify_DimensionVariation(0.02));
+}
+
+TEST(DImensionVariation, NOk) { 
+    ASSERT_EQ(CNC_CheckMachine, Notify_DimensionVariation(0.06));
+}
+
 
 
 int main(int argc, char **argv) {
