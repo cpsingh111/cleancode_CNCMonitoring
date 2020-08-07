e593f003ad9b261481bc61beb095ffd5e84424bf
cpsingh111 <68427957+cpsingh111@users.noreply.github.com>
2020-08-07 14:06:35 +0530

    Array for parameter list

--- ICNC.hpp
+++ ICNC.hpp
@@ -11,25 +11,38 @@ typedef enum
     CNC_CheckMachine_Environment
 }CNC_Status;
 
+typedef enum 
+{
+    StartUpStatusflag,
+    OperatingTempflag,
+    DimensionVariationflag,
+    OperatingHourflag,
+    Param_List_end
+}CNC_Param_List;
+
 CNC_Status Report_Diagnosis();
 
 class ICNC
 {
 private:
     CNC_Status CNC_LED;
+    static bool CNC_Param_flag[Param_List_end];
 public:
-    static bool StartUpStatusflag;
-    static bool OperatingTempflag;
-    static bool DimensionVariationflag;
-    static bool OperatingHourflag;
+
+    bool Get_CNC_Parameter_flag(CNC_Param_List param)
+    {
+        return CNC_Param_flag[param];
+    }
+    void Set_CNC_Parameter_flag(CNC_Param_List param, bool val){
+        CNC_Param_flag[param]=val;
+    }
 
     ICNC()
     {
        CNC_LED = CNC_Ok;
-       StartUpStatusflag=false;
-       OperatingTempflag=false;
-       OperatingHourflag=false;
-       DimensionVariationflag=false;
+       for(int i=0;i<Param_List_end;i++){
+           CNC_Param_flag[i]= false;
+       }
     }
 
 };
